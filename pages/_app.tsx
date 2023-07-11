a7817bb85ee918fd456766a06ee5cc58bccd865f
Dayo Adebanjo <dahyor@outlook.com>
2023-07-11 11:49:52 +0100

    cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,14 +36,7 @@ export default function App({ Component, pageProps, emotionCache = clientSideEmo
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+
         <title>DevUtils</title>
       </Head>
       <StoreProvider store={store}>
